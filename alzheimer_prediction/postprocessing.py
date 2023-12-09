1fe4c348c29badbcb80622a4bdb92dca3cb84f97
CleoY <77142560+CleoY@users.noreply.github.com>
2023-12-09 00:07:38 -0800

    Reverted postprocessing code and png.

--- alzheimer_prediction/postprocessing.py
+++ alzheimer_prediction/postprocessing.py
@@ -23,21 +23,12 @@ def plot_2D_SVM(x_train, y_train, seed):
     plt.contour(xx, yy, Z, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
 
     # Plot support vectors
-    scatter = plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=100, facecolors='none', edgecolors='k')
+    plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=100, facecolors='none', edgecolors='k')
 
     # Plot decision hyperplane
-    print("w[1]")
-    print(w[1])
-
-    # Check if w[1] is close to zero
-    if np.abs(w[1]) > 1e-10:
-        a = -w[0] / w[1]
-        xx_hyperplane = np.linspace(x_train['CDR'].min(), x_train['CDR'].max())
-        yy_hyperplane = a * xx_hyperplane - (clf.intercept_[0]) / w[1]
-    else:
-        # Set x-values to a constant (vertical line)
-        xx_hyperplane = np.full_like(xx_hyperplane, np.mean(x_train['CDR']))
-        yy_hyperplane = np.linspace(x_train['CDR'].min(), x_train['CDR'].max())
+    a = -w[0] / w[1]
+    xx_hyperplane = np.linspace(x_train['CDR'].min(), x_train['CDR'].max())
+    yy_hyperplane = a * xx_hyperplane - (clf.intercept_[0]) / w[1]
 
     plt.plot(xx_hyperplane, yy_hyperplane, 'k-')
 
@@ -51,12 +42,9 @@ def plot_2D_SVM(x_train, y_train, seed):
     plt.plot(xx_hyperplane, yy_bottom, 'k--')
 
     # Name title and axis labels
-    plt.title('Dementia Classification SVM')
-    plt.xlabel('Clinical Dementia Rating')
-    plt.ylabel('Mini-Mental State Examination')
-
-    # Check color coding for classes
-    # plt.colorbar(scatter, label='Class')
+    plt.title('SVM')
+    plt.xlabel('CDR')
+    plt.ylabel('MMSE')
 
     plt.savefig('svm.png')
     plt.show()
